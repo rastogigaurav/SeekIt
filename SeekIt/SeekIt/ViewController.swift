e0167842c08994c2e5d47187986566f64df2195a
Gaurav Rastogi <g.b.rastogi@accenture.com>
2024-09-28 21:39:32 +0800

    Add initial project to the repository
    This is the initial commit to add the ios project to the repository

---
+++ SeekIt/SeekIt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeekIt
+//
+//  Created by g.b.rastogi on 28/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
